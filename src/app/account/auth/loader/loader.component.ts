53fbe33c5e537389fba1653f2468d5cb633babd6
shubham karad <shubham.karad12@gmail.com>
2022-11-29 13:14:06 +0530

    Loader Added

---
+++ src/app/account/auth/loader/loader.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit,Input,ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'app-loader',
+  templateUrl: './loader.component.html',
+  styleUrls: ['./loader.component.scss']
+})
+export class LoaderComponent implements OnInit {
+@Input() bgColor;
+  
+  constructor(private element:ElementRef) { }
+
+  ngOnInit(): void {
+    this.element.nativeElement.style.setProperty('--myvar', `${this.bgColor}`)
+    console.log(this.bgColor);
+  }
+
+}
